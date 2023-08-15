007ce2d11e7354319a16cac4ae57ed9044133870
Alan Hummel <ahummel@abim.org>
2023-08-15 15:24:15 -0400

    Renamed folders

---
+++ api/MyMediaLibraryAPI/Interfaces/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMediaLibrary.API.Interfaces
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string userId, string username, string role);
+    }
+}
---
+++ api/MyMediaLibraryAPI/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MyMediaLibrary.API.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyMediaLibrary.API.Services
+{
+    public class AuthService : IAuthService
+    {
+        public string GenerateJwtToken(string userId, string username, string role)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("the-library"); //TODO: Replace with config-driven value
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId),
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
